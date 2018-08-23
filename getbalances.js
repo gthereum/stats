00caf4c3e046310c5fe16f92b9768be78625d6bc
Philip Dhingra <philipkd@gmail.com>
2018-08-22 17:31:38 -0700

    writes balances now

---
+++ getbalances.js
@@ -0,0 +1,41 @@
+const Web3 = require('web3')
+const fs = require('fs')
+
+const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
+
+let accountsFile = 'data/accounts.txt'
+let balancesFile = 'data/balances.txt'
+
+let accounts = []
+let balances = {}
+
+let write = async () => {
+  fs.writeFileSync(balancesFile,JSON.stringify(balances))
+  console.log("wrote data")
+}
+
+let run = async () => {
+
+  if (fs.existsSync(accountsFile)) {
+    contents = fs.readFileSync(accountsFile,'UTF-8').toString().split("\n")
+    for (line of contents) {
+      accounts.push(line)
+    }
+  }
+
+  for (account of accounts) {
+
+    try {
+      let balance = await web3.eth.getBalance(account)
+      if (balance > 0) {
+        balances[account] = balance
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  write()
+}
+
+run()
